28436809972cf740df9026a8a95cd2692231e0fb
ox1s <linamel08v@gmail.com>
2026-04-25 14:52:07 +0300

    refactoring: best names for chars, and functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,123 @@
 
 #include <DIYables_LCD_I2C.h>
 
-uint8_t hand_1[8] = {
-    0b00000, 0b00011, 0b01000, 0b00100, 0b01000, 0b00100, 0b00010, 0b00001};
+uint8_t hand_first_part[8] = {
+    0b00000,
+    0b00011,
+    0b01000,
+    0b00100,
+    0b01000,
+    0b00100,
+    0b00010,
+    0b00001};
 
-uint8_t hand_2[8] = {
-    0b00000, 0b00000, 0b10000, 0b01000, 0b00100, 0b00000, 0b00000, 0b00000};
+uint8_t hand_second_part[8] = {
+    0b00000,
+    0b00000,
+    0b10000,
+    0b01000,
+    0b00100,
+    0b00000,
+    0b00000,
+    0b00000};
 
-uint8_t hand_down[8] = {
-    0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b01111, 0b10000};
+uint8_t hand_first_part_down[8] = {
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b01111,
+    0b10000};
 
-uint8_t ear_1[8] = {
-    0b00000, 0b01110, 0b00101, 0b01010, 0b01010, 0b10011, 0b10001, 0b10001};
+uint8_t ear_first_part[8] = {
+    0b00000,
+    0b01110,
+    0b00101,
+    0b01010,
+    0b01010,
+    0b10011,
+    0b10001,
+    0b10001};
 
-uint8_t ear_2[8] = {
-    0b00000, 0b00000, 0b00000, 0b10000, 0b01000, 0b01000, 0b00100, 0b00000};
+uint8_t ear_second_part[8] = {
+    0b00000,
+    0b00000,
+    0b00000,
+    0b10000,
+    0b01000,
+    0b01000,
+    0b00100,
+    0b00000};
 
 uint8_t eye[8] = {
-    0b00000, 0b00000, 0b00110, 0b01001, 0b00000, 0b00000, 0b00000, 0b00000};
+    0b00000,
+    0b00000,
+    0b00110,
+    0b01001,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000};
+
+uint8_t hand_second_part_down[8] = {
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b11000,
+    0b00100};
 
 uint8_t back[8] = {
-    0b00000, 0b10000, 0b01000, 0b00100, 0b00010, 0b00010, 0b00001, 0b00001};
-
-uint8_t hand_back[8] = {
-    0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b11000, 0b00100};
+    0b00000,
+    0b10000,
+    0b01000,
+    0b00100,
+    0b00010,
+    0b00010,
+    0b00001,
+    0b00001};
 
 DIYables_LCD_I2C lcd(0x27, 16, 2);
 
 int left = 1;
+
+void createChars()
+{
+  lcd.createChar(0, hand_first_part);
+  lcd.createChar(1, hand_second_part);
+  lcd.createChar(2, ear_first_part);
+  lcd.createChar(3, ear_second_part);
+  lcd.createChar(4, eye);
+  lcd.createChar(6, hand_second_part_down);
+  lcd.createChar(7, hand_first_part_down);
+}
+void printHandDown()
+{
+  lcd.write(7);
+  lcd.write(6);
+}
+
+void printHandUp()
+{
+  lcd.write(0);
+  lcd.write(1);
+}
+
+void printEar()
+{
+  lcd.write(2);
+  lcd.write(3);
+}
+
+void printEye()
+{
+  lcd.write(4);
+}
+
 void setup()
 {
   lcd.init();
@@ -36,39 +126,22 @@ void setup()
 
   Serial.begin(115200);
 
-  lcd.createChar(0, hand_1);
-  lcd.createChar(1, hand_2);
-  lcd.createChar(2, ear_1);
-  lcd.createChar(3, ear_2);
-  lcd.createChar(4, eye);
-  lcd.createChar(6, hand_back);
-  lcd.createChar(7, hand_down);
+  createChars();
 
-  // left hand
   lcd.setCursor(1, 1);
-  lcd.write(7);
-  lcd.write(6);
-
-  // right hand
+  printHandDown();
   lcd.setCursor(6, 1);
-  lcd.write(0);
-  lcd.write(1);
+  printHandUp();
 
-  // ears
   lcd.setCursor(7, 0);
-  lcd.write(2);
-  lcd.write(3);
+  printEar();
   lcd.setCursor(3, 0);
-  lcd.write(2);
-  lcd.write(3);
+  printEar();
 
-  // eyes
   lcd.setCursor(3, 1);
-  lcd.write(4);
-
-  // eyes
+  printEye();
   lcd.setCursor(5, 1);
-  lcd.write(4);
+  printEye();
 }
 
 void loop()
